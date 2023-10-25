431f92f683b552536942f1fa41665bf6b245e16b
Andrew <andrewshmorhun@gmail.com>
2023-10-25 19:46:57 +0300

    add dummy components

---
+++ src/tabs/DummyChart.tsx
@@ -0,0 +1,8 @@
+export default function DummyChart() {
+  return (
+    <div>
+      <h2>Dummy Chart</h2>
+      {/* Chart */}
+    </div>
+  );
+}
---
+++ src/tabs/DummyList.tsx
@@ -0,0 +1,9 @@
+export default function DummyList() {
+  return (
+    <ul>
+      <li>Dummy</li>
+      <li>List</li>
+      {/* List items */}
+    </ul>
+  );
+}
